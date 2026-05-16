b228da9b4fc7d26029d1e2b5610aca41c5d60987
Victor-Vechi <victorhvechi@gmail.com>
2026-05-16 14:13:48 -0300

    refator: remove unused imports

--- src/account/application/core/validator/transfer.validator.ts
+++ src/account/application/core/validator/transfer.validator.ts
@@ -1,5 +1,4 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
-import { DepositValidatorInterface } from "src/account/domain/core/validator/deposit-validator.interface";
 import { EventDto } from "src/account/domain/core/dto/event.dto";
 import { TransferValidatorInterface } from "src/account/domain/core/validator/transfer-validator.interface";
 
--- src/account/domain/core/event/transfer-response.interface.ts
+++ src/account/domain/core/event/transfer-response.interface.ts
@@ -1,6 +1,4 @@
-import { DepositResponseInterface } from "./deposit-response.interface";
 import { EventResponseInterface } from "./event-response.interface";
-import { WithdrawResponseInterface } from "./withdraw-response.interface";
 
 
 export interface TransferResponseInterface extends EventResponseInterface {
